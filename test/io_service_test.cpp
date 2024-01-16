b15e32679aa93637508c7eb0e46ae42cf68e1ba1
vafo <v.safokhujaev@student.inha.uz>
2024-01-16 17:10:17 +0500

    vector of invocables test

--- test/io_service_test.cpp
+++ test/io_service_test.cpp
@@ -382,6 +382,7 @@ TEST_CASE("make_invocable") {
 	auto func = [] (int a, int b) -> int {
 		return a - b;
 	};
+	const int answer = var1 - var2;
 
 	SECTION("move cstr") {
 		std::packaged_task<int(int,int)> task(func);
@@ -391,7 +392,7 @@ TEST_CASE("make_invocable") {
 		std::jthread tr(std::move(inv));
 		ANNOTATE_HAPPENS_BEFORE(&fut);
 		fut.wait();
-		REQUIRE(fut.get() == (var1 - var2));
+		REQUIRE(fut.get() == answer);
 	}
 
 	SECTION("move assignment") {
@@ -402,7 +403,40 @@ TEST_CASE("make_invocable") {
 
 		std::jthread tr(std::move(inv));
 		fut.wait();
-		REQUIRE(fut.get() == (var1 - var2));
+		REQUIRE(fut.get() == answer);
+	}
+
+	SECTION("vector of invocables") {
+		const int iterations = 10;
+		std::vector<invocable> ivec;
+		std::vector<std::future<int>> fvec;
+		for(int i = 0; i < iterations; ++i) {
+			std::packaged_task<int(int,int)> task(func);
+			fvec.push_back(task.get_future());
+			ivec.push_back(
+				invocable(
+					std::move(task),
+					var1, var2));
+		}
+
+		typedef std::vector<invocable>::iterator ivec_iter;
+		auto exec_invocables = 
+			[] (ivec_iter begin, ivec_iter end) {
+				for(; begin != end; ++begin)
+					(*begin)();
+			};
+
+		typedef std::vector<std::future<int>>::iterator fvec_iter;
+		auto check_futures =
+			[] (fvec_iter begin, fvec_iter end) {
+				for(; begin != end; ++begin) {
+					begin->wait();
+					REQUIRE(begin->get() == (answer));
+				}
+			};
+
+		std::jthread tr1(exec_invocables, ivec.begin(), ivec.end());
+		std::jthread tr2(check_futures, fvec.begin(), fvec.end());
 	}
 }
 
