1d0cdee280976af639817ae951ac32adcdde261a
vafo <v.safokhujaev@student.inha.uz>
2024-01-25 11:40:49 +0500

    strand test: dispatch into non-running strand

--- test/strand_test.cpp
+++ test/strand_test.cpp
@@ -121,36 +121,43 @@ TEST_CASE("strand exclusive access") {
 }
 
 template<typename Processor>
-struct exclusive_counter
-    : public exclusive_data<Processor> {
+struct exclusive_counter {
 public:
+    concurrency::recursive_mutex exclusion;
     int counter;
+    strand<Processor> handlers;
+
+    std::atomic<int> increments_total;
 
 public:
     exclusive_counter(Processor& proc)
-        : exclusive_data<Processor>(proc)
+        : counter(0)
+        , handlers(proc)
+        , increments_total(0)
     {}
 };
 
-TEST_CASE("strand dispatch") {
+TEST_CASE("strand in running strand") {
     const int num_threads = 10;
     const int num_tasks = 100;
+    const int num_increments = 100;
 
     memory::shared_ptr<io_service> srvc_ptr = 
         memory::make_shared<io_service>();
 
-    std::vector<concurrency::jthread> trs;
-    for(int i = 0; i < num_threads; ++i)
-        trs.push_back(concurrency::jthread(worker_thread, srvc_ptr));
-
     typedef exclusive_counter<io_service> excl_cnt_type;
-    std::shared_ptr<excl_cnt_type> obj_ptr =
+    std::shared_ptr<excl_cnt_type> obj1_ptr =
             std::make_shared<excl_cnt_type>(*srvc_ptr);
 
-    std::atomic<bool> is_exclusive;
+    std::shared_ptr<excl_cnt_type> obj2_ptr =
+            std::make_shared<excl_cnt_type>(*srvc_ptr);
+
+    std::atomic<bool> is_exclusive(true);
+    std::atomic<bool> failed_dispatch(false);
 
     auto incr_task =
-        [&is_exclusive] (std::weak_ptr<excl_cnt_type> excl_ptr) /*weak_ptr, so as to break cycle*/ {
+        [&is_exclusive, num_increments]
+        (std::weak_ptr<excl_cnt_type> excl_ptr) /*weak_ptr, so as to break cycle*/ {
             using namespace concurrency;
 
             if(excl_ptr.expired())
@@ -159,20 +166,149 @@ TEST_CASE("strand dispatch") {
             std::shared_ptr<excl_cnt_type> obj_ptr = 
                 excl_ptr.lock(); /*get shared_ptr*/
 
-            // since no thread accesses excl_type at same time,
-            // we can try_lock confidently
-            // yet redundant, as no other thread takes part, but at least syncs data among threads
+            // exclusivity check
             if(!obj_ptr->exclusion.try_lock()) {
                 is_exclusive = false;
                 return; 
             }
             
-            lock_guard<mutex> lk(obj_ptr->exclusion, adopt_lock); 
+            lock_guard<recursive_mutex> lk(obj_ptr->exclusion, adopt_lock); 
+            // do increment job
+            for(int i = 0; i < num_increments; ++i)
+                ++obj_ptr->counter;
 
-            // dispatch smth
+            ++obj_ptr->increments_total;
         };
 
+    // worker thread, strand task which dispatches to own strand
+    auto dispatch_own_task =
+        [&is_exclusive, &failed_dispatch, num_increments, incr_task]
+        (std::weak_ptr<excl_cnt_type> excl_ptr) /*weak_ptr, so as to break cycle*/ {
+            using namespace concurrency;
+
+            if(excl_ptr.expired())
+                return;
+
+            std::shared_ptr<excl_cnt_type> obj_ptr = 
+                excl_ptr.lock(); /*get shared_ptr*/
+
+            // exclusivity check
+            if(!obj_ptr->exclusion.try_lock()) {
+                is_exclusive = false;
+                return; 
+            }
+            
+            lock_guard<recursive_mutex> lk(obj_ptr->exclusion, adopt_lock); 
+            // dispatch to own strand
+            int capture_val = obj_ptr->counter;
+
+            // guaranteed to execute exactly once
+            obj_ptr->handlers.dispatch(std::bind(incr_task, excl_ptr));
+
+            int cur_val = obj_ptr->counter;
+            if(cur_val != (capture_val + num_increments)) {
+                failed_dispatch = true;
+            }
+        };
+
+    auto dispatch_tasks =
+        [dispatch_own_task] (
+            std::shared_ptr<excl_cnt_type>& own)
+        {
+            own->handlers.dispatch(
+            std::bind(dispatch_own_task, std::weak_ptr<excl_cnt_type>(own)));
+        };
+
+    for(int i = 0; i < num_tasks; ++i) {
+        dispatch_tasks(obj1_ptr);
+        dispatch_tasks(obj2_ptr);
+    }
+
+    std::vector<concurrency::jthread> trs;
+    for(int i = 0; i < num_threads; ++i)
+        trs.push_back(concurrency::jthread(worker_thread, srvc_ptr));
+
     srvc_ptr->stop();
+
+    REQUIRE(is_exclusive);
+    REQUIRE(!failed_dispatch);
+}
+
+TEST_CASE("dispatch into non-running strand") {
+    const int num_threads = 10;
+    const int num_strand_tasks = 100;
+
+    memory::shared_ptr<io_service> srvc_ptr = 
+        memory::make_shared<io_service>();
+
+    typedef strand<io_service> handles_type;
+    std::shared_ptr<handles_type> strand_ptr =
+        std::make_shared<handles_type>(*srvc_ptr);
+
+    // used for checking strand exclusivity (single thread executing)
+    concurrency::mutex strand_mutex;
+
+    std::atomic<bool> is_exclusive(true);
+    std::atomic<bool> failed_dispatch(false);
+
+    // dispatched into strand
+    auto dispatched_task =
+        [&strand_mutex, &is_exclusive, &failed_dispatch]
+        (std::weak_ptr<handles_type> handle_ptr) {
+            if(handle_ptr.expired())
+                return;
+
+            // exclusivity check
+            if(strand_mutex.try_lock() == false)
+                is_exclusive = false;
+
+            using namespace concurrency;
+            lock_guard<mutex> lk(strand_mutex, adopt_lock);
+            std::shared_ptr<handles_type> obj_ptr(handle_ptr);
+
+            int* counter = 
+                callstack<handles_type, int>::contains(obj_ptr.get());
+
+            // if this task is not called by dispatch
+            if(counter == nullptr) {
+                failed_dispatch = true;
+                return;
+            }
+
+            // increment dispatched task done
+            ++(*counter);
+        };
+
+    // posted to thread pool
+    auto pool_task =
+        [num_strand_tasks, &failed_dispatch, dispatched_task]
+        (std::weak_ptr<handles_type> handle_ptr) {
+            if(handle_ptr.expired())
+                return;
+            std::shared_ptr<handles_type> obj_ptr(handle_ptr);
+
+            int counter = 0; /*count of tasks executed by dispatch*/
+            callstack<handles_type, int>::context cntx(obj_ptr.get(), counter);
+
+            for(int i = 0; i < num_strand_tasks; ++i)
+                obj_ptr->dispatch(std::bind(dispatched_task, handle_ptr));
+
+            if(counter != num_strand_tasks)
+                failed_dispatch = true;
+        };
+
+    // post one task to thread pool, which will be the only runner of strand
+    // so it can freely dispatch into strand and expect them to run within dispatch
+    srvc_ptr->post(pool_task, std::weak_ptr<handles_type>(strand_ptr));
+
+    std::vector<concurrency::jthread> trs;
+    for(int i = 0; i < num_threads; ++i)
+        trs.push_back(concurrency::jthread(worker_thread, srvc_ptr));
+
+    srvc_ptr->stop();
+
+    REQUIRE(is_exclusive);
+    REQUIRE(!failed_dispatch);
 }
 
 } // namespace io_service
