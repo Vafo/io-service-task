670caca9a5dbbb8e4abaa34ad07d034fd37dc22f
vafo <v.safokhujaev@student.inha.uz>
2024-02-09 17:51:08 +0500

    modified uring_cqe
    Related to #16

--- src/async/uring.hpp
+++ src/async/uring.hpp
@@ -1,9 +1,12 @@
 #ifndef ASIO_URING_HPP
 #define ASIO_URING_HPP
 
+#include <cassert>
+#include <cerrno>
 #include <cstring>
 #include <liburing.h>
 #include <liburing/io_uring.h>
+#include <stdexcept>
 
 #include "cerror_code.hpp"
 
@@ -64,6 +67,9 @@ public:
     io_uring_sqe* get() const
     { return m_sqe_ptr; }
 
+    void set_data(uint64_t data)
+    { io_uring_sqe_set_data64(m_sqe_ptr, data); }
+
 private:
     friend class uring;
 
@@ -71,21 +77,68 @@ private:
 
 class uring_cqe {
 private:
+    io_uring* m_ring_owner;
     io_uring_cqe* m_cqe_ptr;
 
 private:
-    uring_cqe() = delete;
     uring_cqe(const uring_cqe&) = delete;
     uring_cqe& operator=(const uring_cqe&) = delete;
 
-private:
-    uring_cqe(io_uring_cqe* cqe_ptr)
-        : m_cqe_ptr(cqe_ptr)
+public:
+    uring_cqe()
+        : m_ring_owner()
+        , m_cqe_ptr()
     {}
 
+// Used by uring
+private:
+    uring_cqe(io_uring& owner, io_uring_cqe* cqe_ptr)
+        : m_ring_owner(&owner)
+        , m_cqe_ptr(cqe_ptr)
+    {}
+
+    uring_cqe(uring_cqe&& other)
+        : m_ring_owner(std::move(other.m_ring_owner))
+        , m_cqe_ptr(std::move(other.m_cqe_ptr))
+    {}
+
+    uring_cqe& operator=(uring_cqe&& other) {
+        uring_cqe(std::move(other)).swap(*this);
+        return *this;
+    }
+
 public:
-    io_uring_cqe* get() const
-    { return m_cqe_ptr; }
+    ~uring_cqe() {
+        if(*this) // if valid
+            io_uring_cqe_seen(m_ring_owner, m_cqe_ptr);
+    }
+
+public:
+    int get_res() const {
+        M_check_validity();
+        return m_cqe_ptr->res;
+    }
+
+    uint64_t get_data() const {
+        M_check_validity();
+        return io_uring_cqe_get_data64(m_cqe_ptr);
+    }
+
+    operator bool()
+    { return m_ring_owner && m_cqe_ptr; }
+
+public:
+    void swap(uring_cqe& other) {
+        using std::swap;
+        swap(m_ring_owner, other.m_ring_owner);
+        swap(m_cqe_ptr, other.m_cqe_ptr);
+    }
+
+private:
+    void M_check_validity() const {
+        if(!m_cqe_ptr || !m_ring_owner)
+            throw std::runtime_error("uring_cqe is empty");
+    }
 
 private:
     friend class uring;
@@ -131,6 +184,24 @@ public:
         }
 
         return uring_sqe(sqe);
+    }
+
+    bool try_get_cqe(uring_cqe& cqe) {
+        uring_error err;
+        io_uring_cqe* cqe_ptr = NULL;
+        err = io_uring_peek_cqe(&m_ring, &cqe_ptr);
+
+        if(err) {
+            if (err.value() == -EAGAIN) {
+                return false;
+            } else {
+                err.throw_exception();
+            }
+        }
+        
+        assert(cqe_ptr != NULL && "cqe_ptr is null");
+        cqe = uring_cqe(m_ring, cqe_ptr);
+        return true;
     }
 
 public:
