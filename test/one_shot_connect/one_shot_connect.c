fa51fec228de5fd33d622fc35e127f49d46dd4fe
vafo <v.safokhujaev@student.inha.uz>
2024-02-13 16:17:25 +0500

    acceptor tests & one_shot_connect test program
    Related to #16

---
+++ test/one_shot_connect/one_shot_connect.c
@@ -0,0 +1,86 @@
+#include <netdb.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int setup_connect(char* addr, char* port, struct sockaddr* out_addr, socklen_t* out_len) {
+    struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+        .ai_protocol = 0,
+        .ai_flags = AI_NUMERICSERV
+    };
+
+    struct addrinfo* results;
+    int ret;
+
+    ret = getaddrinfo(addr, port, &hints, &results);
+
+    if(ret != 0) {
+        printf("could not get addrinfo %s %s: %s\n", addr, port, gai_strerror(ret));
+        return -1;
+    }
+
+    struct addrinfo* adrp;
+    int sock = -1;
+    for(adrp = results; adrp != NULL; adrp = adrp->ai_next) {
+        sock = socket(adrp->ai_family, adrp->ai_socktype, adrp->ai_protocol);
+        if(sock < 0) {
+            continue;
+        }
+        break;
+    }
+
+    if(sock < 0) {
+        printf("could not create socket: %s\n", strerror(errno));
+        return -1;
+    }
+
+    if(adrp == NULL) {
+        printf("could not find appropriate host %s %s\n", addr, port);
+        return -1;
+    }
+
+    if(adrp->ai_addrlen > *out_len) {
+        printf("given length is small\n");
+        close(sock);
+        return -1;
+    }
+
+    memcpy(out_addr, adrp->ai_addr, adrp->ai_addrlen);
+    *out_len = adrp->ai_addrlen;
+
+    freeaddrinfo(results);
+    return sock;
+}
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 3) {
+        printf("Usage: %s hostname port\n", argv[0]);
+        exit(1);
+    }
+
+    struct sockaddr addr;
+    socklen_t len = sizeof(addr);
+    int sock_fd = setup_connect(argv[1], argv[2], &addr, &len);
+
+    if(sock_fd == -1) {
+        printf("could not setup connection\n");
+        exit(1);
+    }
+
+    int err =
+        connect(sock_fd, &addr, len);
+
+    if(err) {
+        printf("could not connect: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    return 0;
+}
--- test/unit/async/uring_test.cpp
+++ test/unit/async/uring_test.cpp
@@ -1,5 +1,88 @@
 #include <catch2/catch_all.hpp>
 
-TEST_CASE("keka") {
+#include <iostream>
 
+#include "io_service.hpp"
+#include "condition_variable"
+#include "socket.hpp"
+#include "unique_lock.hpp"
+#include "mutex.hpp"
+#include "jthread.hpp"
+
+#include "acceptor.hpp"
+#include "mutex.hpp"
+
+namespace io_service {
+
+static void worker_func(io_service* serv_ptr) {
+    try
+    {
+        serv_ptr->run();
+    }
+    catch(const service_stopped_error& e)
+    {
+        // Should run() continue or abort (?)
+        // std::cerr << e.what() << '\n';
+    }
+    catch(const std::runtime_error& e) {
+        std::cerr << e.what() << '\n';
+        REQUIRE(false); /*worker thread has unhandled exception*/
+    }
 }
+
+TEST_CASE("acceptor creation") {
+    const int port_num = 9999;
+    const int num_cons = 10;
+
+    io_service serv;
+
+    ip::acceptor ac(serv);
+
+    REQUIRE_NOTHROW(
+        ac.bind(port_num));
+
+    REQUIRE_NOTHROW(
+        ac.listen(num_cons));
+}
+
+TEST_CASE("io_service & acceptor") {
+    const int num_threads = 10;
+    const int port_num = 9999;
+    const int num_cons = 10;
+    io_service serv;
+    std::vector<concurrency::jthread> threads;
+    
+    for(int i = 0; i < num_threads; ++i)
+        threads.emplace_back(worker_func, &serv);
+
+    ip::acceptor ac(serv);
+
+    concurrency::condition_variable cond_var;  
+    concurrency::mutex mut;
+    std::atomic<bool> done(false);
+
+    ac.bind(port_num);
+    ac.listen(num_cons);
+
+    ac.async_accept(
+        [&cond_var, &mut, &done](int err, ip::socket sock) {
+            using namespace concurrency;
+            // std::cout << "AAA" << std::endl;
+            unique_lock<mutex> lk(mut);
+            done = true;
+            cond_var.notify_one();
+        });
+
+    // block until async_accept
+    // std::cout << "IN" << std::endl;
+    {
+        using namespace concurrency;
+        unique_lock<mutex> lk(mut);
+        cond_var.wait(lk, [&done]() { return done.load(); });
+    }
+    // std::cout << "OUT" << std::endl;
+
+    serv.stop();
+}
+
+} // namespace io_service
