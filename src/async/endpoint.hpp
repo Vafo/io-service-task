82a09d6a26cc5e75166ddbea8819cff267feb82d
vafo <v.safokhujaev@student.inha.uz>
2024-02-15 16:00:16 +0500

    endpoint interface for async_connect_init
    Related to #16

--- src/async/endpoint.hpp
+++ src/async/endpoint.hpp
@@ -2,14 +2,21 @@
 #define ASIO_ENDPOINT_HPP
 
 
-#include <experimental/bits/fs_fwd.h>
+#include <cassert>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <tuple>
 
 
 namespace io_service {
 namespace ip {
+
+namespace detail {
+
+// Forward declaration for socket.hpp
+class async_connect_init;
+
+} // namespace detail
+
 
 class endpoint {
 private:
@@ -18,9 +25,9 @@ private:
 
 private:
     // used by resolver
-    endpoint(sockaddr&& addr, socklen_t&& len)
-        : m_addr(std::forward<sockaddr>(addr))
-        , m_len(std::forward<socklen_t>(len)) 
+    endpoint(sockaddr& addr, socklen_t& len)
+        : m_addr(addr)
+        , m_len(len) 
     {}
 
 public:
@@ -30,17 +37,46 @@ public:
     {}
 
     endpoint(in_port_t port)
-    {}
+        : endpoint()
+    {
+        assert(sizeof(sockaddr) >= sizeof(sockaddr_in));
+
+        sockaddr_in* in_addr_ptr = reinterpret_cast<sockaddr_in*>(&m_addr);
+        in_addr_ptr->sin_family = AF_INET;
+        in_addr_ptr->sin_addr.s_addr = INADDR_ANY;
+        in_addr_ptr->sin_port = htons(port);
+        
+        m_len = sizeof(sockaddr_in);
+    }
 
     endpoint(const endpoint& other)
+        : m_addr(other.m_addr)
+        , m_len(other.m_len)
     {}
 
     endpoint& operator=(const endpoint& other) {
+        endpoint(other).swap(*this);
         return *this;
     }
 
+public:
+    void swap(endpoint& other) {
+        using std::swap;
+        swap(m_addr, other.m_addr);
+        swap(m_len, other.m_len);
+    }
+
+private:
+    // Used by async_connect_init
+    sockaddr& get_addr() // TODO: consider returning a copy
+    { return m_addr; }
+
+    socklen_t get_len()
+    { return m_len; }
+
 private:
 friend class resolver;
+friend class detail::async_connect_init;
 
 }; // class endpoint
 
